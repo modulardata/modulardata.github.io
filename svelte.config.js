9debb2d2616a97e1df87004ef15c628d1b9701f4
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-10-17 08:44:50 +0200

    Template Setup Vite Svelte Github-Pages PATH Issue 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
-// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	// preprocess: vitePreprocess(),
+
 	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true
+			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.BASE_PATH || ''
+			base: process.env.NODE_ENV === 'production' ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : ''
 		}
 	}
 };
